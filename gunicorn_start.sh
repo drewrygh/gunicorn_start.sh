68db109c8cdf311a86f18418aee7fc2678100bc6
frankvdp <frankvdp@orange.nl>
2013-10-18 21:47:19 +0200

    init commit

---
+++ gunicorn_start.sh
@@ -0,0 +1,31 @@
+NAME="NAME_APPLICATION"                                                  # Name of the application
+DJANGODIR=/path/to/project/dir                              # Django project directory
+SOCKFILE=/path/to/dir/with/gunicorn.sock      # we will communicte using this unix socket
+SHAREDDIR=/path/to/shared/dir
+USER=username                                           # the user to run as
+GROUP=username                                          # the group to run as
+NUM_WORKERS=3                                                   # how many worker processes should Gunicorn spawn
+DJANGO_SETTINGS_MODULE=config.settings          # which settings file should Django use
+DJANGO_WSGI_MODULE=config.wsgi                  # WSGI module name
+
+echo "Starting $NAME"
+
+# Activate the virtual environment
+cd $DJANGODIR
+source $SHAREDDIR/venv/bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+exec $SHAREDDIR/venv/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user=$USER --group=$GROUP \
+  --log-level=debug \
+  --bind=unix:$SOCKFILE
+
